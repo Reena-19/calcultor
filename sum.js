5419563a406c070293838ffa99cfde536e33edba
Reena-19 <138842205+Reena-19@users.noreply.github.com>
2023-07-31 16:04:35 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,12 @@
+function add(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("button1").innerHTML="Sum of numbers:"+c;
+}
+function sub(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a-b;
+    document.getElementById("button2").innerHTML="Different of numbers:"+c;
+}
